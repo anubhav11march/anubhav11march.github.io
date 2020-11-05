5e25998ef954887372de2b584433c791061c2d93
Anubhav Mittal <42939575+anubhav11march@users.noreply.github.com>
2020-11-05 17:18:32 +0530

    :sparkles: Code 1

---
+++ ct1.java
@@ -0,0 +1,26 @@
+/*
+ * Enter your code here. Read input from STDIN. Print your output to STDOUT.
+ * Your class should be named CandidateCode.
+ */
+
+import java.io.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+public class ct1 {
+
+    public static void main(String args[] ) throws Exception {
+        int res = 0;
+        LocalDate start = LocalDate.of(1901, Month.JANUARY, 1),
+                 end = LocalDate.of(2001, Month.JANUARY, 1);
+        while(start.isBefore(end)){
+            if(start.getDayOfWeek() == DayOfWeek.SUNDAY)
+                res++;
+            start = start.plusMonths(1);
+        }
+        System.out.println(res);
+    }
+
+
+}
